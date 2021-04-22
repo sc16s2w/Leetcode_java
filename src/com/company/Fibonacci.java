4ff13fe79c97e7e0701bd386a8446b8a2008901d
sc16s2w <765012395@qq.com>
2021-04-22 11:18:22 +0200

    第三题更新“ q
    q
    
    echo Hello

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -1,7 +1,52 @@
 package com.company;
 
+import static java.lang.Math.sqrt;
+
 public class Fibonacci {
+    //带入公式
     public int Fibonacci(int n) {
-        
+        if(n < 3){
+            if(n == 0){
+                int result = 0;
+                return result;
+            }
+            else{
+                int result = 1;
+                return result;
+            }
+
+        }
+        else{
+            int result = (int) (1/sqrt(5)*(Math.pow((1+sqrt(5))/2,n) - Math.pow((1-sqrt(5))/2,n)));
+            return result;
+        }
+
     }
+
+    //利用通项公式
+    public int Fibonacci_2nd(int n) {
+        if(n < 3){
+            if(n == 0){
+                int result = 0;
+                return result;
+            }
+            else{
+                int result = 1;
+                return result;
+            }
+        }
+        else{
+            int temp = 0;
+            int x = 1;
+            int y = 1;
+            for(int i =3; i<=n;i++){
+                temp = x+y;
+                x = y;
+                y = temp;
+            }
+            return temp;
+        }
+
+    }
+
 }
