8899d97b7099f269e05e962e3827ef8ba639b8b0
sc16s2w <765012395@qq.com>
2021-04-29 22:20:16 +0200

    完全二叉树“ eche hello

--- src/com/company/Common_parent_node.java
+++ src/com/company/Common_parent_node.java
@@ -1,5 +1,32 @@
 package com.company;
 
-public class Common_parent_node {
+import java.util.Scanner;
 
+/**
+ * 如上图所示，由正整数 1, 2, 3, ...组成了一棵无限大的二叉树。
+ * 从某一个结点到根结点（编号是1的结点）都有一条唯一的路径，比如从5到根结点的路径是（5, 2, 1），
+ * 从4到根结点的路径是（4, 2, 1），从根结点1到根结点的路径上只包含一个结点1，因此路径就是（1）。
+ * 对于两个结点x和y，假设他们到根结点的路径分别是（x1, x2, ... ,1）和（y1, y2,...,1），
+ * 那么必然存在两个正整数i和j，使得从xi 和yj 开始，有xi = yj，xi + 1 = yj + 1，xi + 2 = yj + 2，...
+ * 现在的问题就是，给定x和y，要求他们的公共父节点，即xi（也就是 yj）。
+ */
+//完全二叉树 不用hashmap处理
+public class Common_parent_node {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            while(x!=y){
+                if(x>=y){
+                    x = x/2;
+                }
+                else{
+                    y = y/2;
+                }
+            }
+            System.out.println(x);
+        }
+
+    }
 }
